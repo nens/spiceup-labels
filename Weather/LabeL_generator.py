41ee2b0ffe52bc8901c291618628f7a7f16baa65
mdkrol <46001151+mdkrol@users.noreply.github.com>
2020-05-22 17:32:24 +0200

    Update weather labeltypes

--- Weather/LabeL_generator.py
+++ Weather/LabeL_generator.py
@@ -7,6 +7,17 @@ Created on Thu Jan  2 11:48:05 2020
 
 import json
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+if not 'weather_info' in locals():
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    sh = client.open("Items & Properties on the App Ui/x")
+    ws = sh.worksheet("Weather")
+    weather_info = pd.DataFrame(ws.get_all_records())
+    weather_info = weather_info[weather_info["parameter"]!="Location"]
 
 with open('Labels_basis.json') as json_file:
     data = json.load(json_file)
@@ -14,8 +25,6 @@ with open('Labels_basis.json') as json_file:
 source = data["source"]
 graph = source["graph"]
 result = graph["result"]
-
-params = pd.read_csv("params.csv")
     
 #%%
 
@@ -26,8 +35,8 @@ def create_lizardrastersource(code,uuid):
 
 def create_aggregate(code):
     key = "{}_aggregate".format(code)
-    method = "max" if code.startswith("icon") else "mean"  #Icon mag niet middelen dus pakt max
-    value = ["geoblocks.geometry.aggregate.AggregateRaster","boundaries",code,method,"epsg:3857",100,None,"{}_label".format(code)]
+    method = "max" if code.endswith("summary") else "mean"  #Summary mag niet middelen dus pakt max
+    value = ["geoblocks.geometry.aggregate.AggregateRaster","parcels",code,method,"epsg:4326",0.00001,None,"{}_label".format(code)]
     return {key:value}
         
 def create_seriesblock(code):
@@ -50,17 +59,16 @@ def update_result(code,label,result):
 
 #%%
 
-for index, row in params.iterrows():
-    code = row["code"]
-    uuid = row["uuid"]
-    label = row["label"]
+for index, row in weather_info.iterrows():
+    code = row["parameter"]
+    code = code.replace(" ","_").replace("(","").replace(")","").lower()
+    uuid = row["Raster UUID"]
     rastersource = create_lizardrastersource(code,uuid)
     graph.update(rastersource)
     aggregate = create_aggregate(code)
     graph.update(aggregate)
     seriesblock = create_seriesblock(code)
     graph.update(seriesblock)
-    result = update_result(code,"{}_t0".format(label),result)
     
     shifts = create_shifts(code)
     graph.update(shifts)
@@ -70,7 +78,7 @@ for index, row in params.iterrows():
         graph.update(aggregate)
         seriesblock = create_seriesblock(key)
         graph.update(seriesblock)
-        shiftlabel = "{}_t{}".format(label,key.split("_")[-1])
+        shiftlabel = "{}_t{}".format(code,key.split("_")[-1])
         result = update_result(key,shiftlabel,result)
 
 graph["result"]=result  
--- Weather/LabeL_generator_startup.py
+++ Weather/LabeL_generator_startup.py
@@ -5,15 +5,19 @@ Created on Fri Jan  3 11:08:35 2020
 @author: martijn.krol
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  2 11:48:05 2020
-
-@author: martijn.krol
-"""
-
 import json
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+if not 'weather_info' in locals():
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    sh = client.open("Items & Properties on the App Ui/x")
+    ws = sh.worksheet("Weather")
+    weather_info = pd.DataFrame(ws.get_all_records())
+    weather_info = weather_info[weather_info["parameter"]!="Location"]
 
 with open('Labels_basis.json') as json_file:
     data = json.load(json_file)
@@ -21,8 +25,6 @@ with open('Labels_basis.json') as json_file:
 source = data["source"]
 graph = source["graph"]
 result = graph["result"]
-
-params = pd.read_csv("params_startup.csv")
     
 #%%
 
@@ -34,7 +36,7 @@ def create_lizardrastersource(code,uuid):
 def create_aggregate(code):
     key = "{}_aggregate".format(code)
     method = "max" if code.startswith("icon") else "mean"  #Icon mag niet middelen dus pakt max
-    value = ["geoblocks.geometry.aggregate.AggregateRaster","boundaries",code,method,"epsg:3857",100,None,"{}_label".format(code)]
+    value = ["geoblocks.geometry.aggregate.AggregateRaster","parcels",code,method,"epsg:4326",0.00001,None,"{}_label".format(code)]
     return {key:value}
         
 def create_seriesblock(code):
@@ -49,17 +51,17 @@ def update_result(code,label,result):
 
 #%%
 
-for index, row in params.iterrows():
-    code = row["code"]
-    uuid = row["uuid"]
-    label = row["label"]
+for index, row in weather_info.iterrows():
+    code = row["parameter"]
+    code = code.replace(" ","_").replace("(","").replace(")","").lower()
+    uuid = row["Raster UUID"]
     rastersource = create_lizardrastersource(code,uuid)
     graph.update(rastersource)
     aggregate = create_aggregate(code)
     graph.update(aggregate)
     seriesblock = create_seriesblock(code)
     graph.update(seriesblock)
-    result = update_result(code,"{}_t0".format(label),result)
+    result = update_result(code,"{}_t0".format(code),result)
 
 graph["result"]=result  
 source["graph"]=graph
